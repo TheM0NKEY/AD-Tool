8ae69c4b3cd314c5dc5a7ab6012f4512ad43ef1f
TheM0nkey <jack.williams497@gmail.com>
2026-05-21 22:46:21 +0100

    feat: add Step2ValidateView with status icons and invalid row warning

--- ADTool/Views/Step2ValidateView.xaml.cs
+++ ADTool/Views/Step2ValidateView.xaml.cs
@@ -1,3 +1,18 @@
+using ADTool.ViewModels;
 using System.Windows.Controls;
+
 namespace ADTool.Views;
-public partial class Step2ValidateView : UserControl { public Step2ValidateView() { InitializeComponent(); } }
+
+public partial class Step2ValidateView : UserControl
+{
+    public Step2ValidateView()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (DataContext is Step2ValidateViewModel vm)
+            await vm.ValidateAllAsync();
+    }
+}

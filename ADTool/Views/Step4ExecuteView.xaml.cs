b738ae790691e51b23fb50c37b147ea7fafdd802
TheM0nkey <jack.williams497@gmail.com>
2026-05-21 22:47:05 +0100

    feat: add Step4ExecuteView with inline error expanders and export

--- ADTool/Views/Step4ExecuteView.xaml.cs
+++ ADTool/Views/Step4ExecuteView.xaml.cs
@@ -1,3 +1,18 @@
+using ADTool.ViewModels;
 using System.Windows.Controls;
+
 namespace ADTool.Views;
-public partial class Step4ExecuteView : UserControl { public Step4ExecuteView() { InitializeComponent(); } }
+
+public partial class Step4ExecuteView : UserControl
+{
+    public Step4ExecuteView()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        if (DataContext is Step4ExecuteViewModel vm)
+            await vm.ExecuteAllAsync();
+    }
+}
